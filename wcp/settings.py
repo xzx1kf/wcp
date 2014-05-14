89a564e8df2a14aebcddfa778865e83dea3f7a94
= <=>
2014-05-14 13:37:29 +0100

    settings.py update

--- wcp/settings.py
+++ wcp/settings.py
@@ -55,7 +55,7 @@ WSGI_APPLICATION = 'wcp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-import dj_databse_url
+import dj_database_url
 DATABASES = {'default': dj_database_url.config()}
 #DATABASES = {
 #    'default': {
